de79a1f0c949c75cf01bbfa0e9da23cbf3634e59
issam <is.elouafa@aulanz.net>
2022-11-08 14:04:45 +0100

    primera subida de php

---
+++ formulario.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+<?php
+    print("prueba");
+    $nombre=$_POST["nombre"];
+    print($nombre);
+    $eventoprog = $_POST["subeventos"];
+    print($eventoprog);
+    $edad=$_POST["edad"];
+?>   
+
+</body>
+</html>
